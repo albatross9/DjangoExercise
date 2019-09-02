3d7c415fd75207347bfa61b70159b0766a31dbd6
Albatross9 <albatross918@daum.net>
2019-09-03 02:02:04 +0900

    유저 앱,모델등록

--- amsuser/admin.py
+++ amsuser/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Amsuser
 
 # Register your models here.
+
+
+class AmsAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Amsuser, AmsAdmin)

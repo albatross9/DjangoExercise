70bd6646c459ce5b0feda7aa0e4da7aa42fcab01
Albatross9 <albatross918@daum.net>
2019-09-05 06:46:09 +0900

    오류수정

--- amscommunity/urls.py
+++ amscommunity/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('amsuser/', include('amsuser.urls'))
+    path('amsuser/', include('amsuser.urls')),
 ]

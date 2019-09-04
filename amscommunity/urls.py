8abd82f0fa4ce5409535295d5575b6bc85ec7660
Albatross9 <albatross918@daum.net>
2019-09-05 06:05:10 +0900

    철자틀림

--- amscommunity/urls.py
+++ amscommunity/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('amsuser/', include('amsuser.urls'))  # ams아래의 모든것을은 ams url이 관리하게해줌
+    path('amsuser/', include('amsuser.urls'))
 ]

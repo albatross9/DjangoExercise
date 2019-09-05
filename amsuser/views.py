6134403080b8dc83559c808528eaf99bd219b831
Albatross9 <albatross918@daum.net>
2019-09-05 19:41:39 +0900

    비밀번호가 다를떄 메세지

--- amsuser/views.py
+++ amsuser/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Amsuser
 
@@ -8,16 +9,21 @@ def register(request):
 
     if request.method == 'GET':
         return render(request, 'register.html')
+
     elif request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
         re_password = request.POST['re-password']
 
-        amsuser = Amsuser(
-            username=username,
-            password=password
-        )
+        res_data = {}
+        if password != re_password:
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            amsuser = Amsuser(
+                username=username,
+                password=password
+            )
 
-        amsuser.save()
+            amsuser.save()
 
-        return render(request, 'register.html')
+        return render(request, 'register.html', res_data)

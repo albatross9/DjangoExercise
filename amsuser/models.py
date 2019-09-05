6705928af0d02ad76bdbd5146587cad939ea76a7
Albatross9 <albatross918@daum.net>
2019-09-06 01:33:10 +0900

    이메일필드 추가

--- amsuser/models.py
+++ amsuser/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Amsuser(models.Model):
     username = models.CharField(max_length=64, verbose_name='사용자명')
-
+    useremail = models.EmailField(max_length=128, verbose_name = '사용자이메일')
     password = models.CharField(max_length=64, verbose_name='비밀번호')
 
     registered_dttm = models.DateTimeField(

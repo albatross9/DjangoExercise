0a697baba39cfcdcfdea17812645ca07f98ca845
Albatross9 <albatross918@daum.net>
2019-09-06 01:21:35 +0900

    회원가입창에 모든란에 항목을 안적었을떄 오류나오게

--- amsuser/views.py
+++ amsuser/views.py
@@ -1,23 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+<<<<<<< HEAD
 from Django.contirb.auth.hashers import make_password
+=======
+from django.contrib.auth.hashers import make_password
+>>>>>>> 회원가입창에 모든란에 항목을 안적었을떄 오류나오게
 from .models import Amsuser
 
 # Create your views here.
 
 
 def register(request):
-
     if request.method == 'GET':
         return render(request, 'register.html')
-
     elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        re_password = request.POST['re-password']
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
 
         res_data = {}
-        if password != re_password:
+
+        if not (username and password and re_password):
+            res_data['error'] = '모든 값을 입력해야합니다.'
+        elif password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             amsuser = Amsuser(

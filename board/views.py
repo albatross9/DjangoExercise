e9e3d2827e391f7051676b2685ce4100209e994a
Albatross9 <albatross918@daum.net>
2019-09-23 23:59:59 +0900

    새로운 앱으로 테스트

--- board/views.py
+++ board/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Board
+from board.forms import BoardForm
 
 # Create your views here.
 
@@ -7,3 +8,8 @@ from .models import Board
 def board_list(request):
     boards = Board.objects.all().order_by('-id')
     return render(request, 'board_list.html', {'boards': boards})
+
+
+def board_write(request):
+    boards = Board.objects.all().order_by('-id')
+    return render(request, 'board_write.html', {'boards': boards})
